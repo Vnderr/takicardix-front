38d7f6c66bba4a4e2398ffc0e652a28c0f699a22
Vnderr <ande.diazp@duocuc.cl>
2025-11-22 17:17:31 -0300

    cambios

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import '../../styles/Navbar.css';
+import '../../styles/navbar.css';
 
 function Navbar({ links, title }) {
   const [isOpen, setIsOpen] = useState(false);
